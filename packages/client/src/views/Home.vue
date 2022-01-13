2dab31b95f49f3bcfe88139a0b145cd1da67d9fa
Krisdina <77259488+krisdinakr@users.noreply.github.com>
2022-01-13 12:06:22 +0700

    [client] delete unused component and fix Home Page

--- packages/client/src/views/Home.vue
+++ packages/client/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section>
+      <Card name="Daniel" accountNumber="1232-2442-4242" :balance="100000" />
+      <CallToAction />
+    </section>
+    <section>
+      <Activity />
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Activity from '@/components/Activity.vue'
+import Card from '@/components/Card.vue'
+import CallToAction from '@/components/CallToAction.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { Activity, Card, CallToAction }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: row;
+    justify-content: space-between;
+    gap: 1.25rem;
+  }
+}
+
+section {
+  width: 100%;
+}
+
+</style>
